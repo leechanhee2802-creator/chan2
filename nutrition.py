c3eeff212c46457d22d22a3e1e2cd941eb2e2b93
leechanhee2802-creator <leechanhee2802@gmail.com>
2025-12-06 19:19:17 +0900

    Update nutrition.py

--- nutrition.py
+++ nutrition.py
@@ -100,3 +100,7 @@ def summarize(foods: Iterable[str]) -> Tuple[List[List[str]], FoodInfo]:
         unit="총합",
     )
     return rows, total
+  def summarize_foods(foods)
+      return summarize(foods)
+
+
